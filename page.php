2efe2c76b971153a1a0947d9be80533263b9f3e5
trvswgnr <trvswgnr@gmail.com>
2018-01-17 16:03:30 -0500

    [added] container class

--- page.php
+++ page.php
@@ -1,13 +1,15 @@
 <?php get_header(); ?>
 
-<?php
-if ( have_posts() ) :
-  while ( have_posts() ) :
-    the_post();
-    the_content();
-?>
-<?php endwhile; else : ?>
+<div class="container">
+  <?php
+  if ( have_posts() ) :
+    while ( have_posts() ) :
+      the_post();
+      the_content();
+  ?>
+  <?php endwhile; else : ?>
 
-<?php endif; ?>
+  <?php endif; ?>
+</div>
 
 <?php get_footer(); ?>
